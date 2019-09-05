84b0d17e26c23735889d9ed83533f6e7994f5f80
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2019-09-05 20:37:09 -0300

    Desenvolvendo aula JWT

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '144.202.38.239',
+  username: 'postgres',
+  password: 'B@lpha9001',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
